da3518ac0ec5ddd3ddc077531d8b2f0b1003a228
alexis1546 <pvaldemaralexis@gmail.com>
2025-03-18 18:44:24 +0100

    programme minute

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Entrez un nb de minutes");
+int nb_min =  int.Parse(Console.ReadLine());
+int[] time = {0,0};
+time[0] = nb_min / 60;
+time[1] = nb_min % 60;
+Console.WriteLine(time[0].ToString()+":"+time[1].ToString() );
