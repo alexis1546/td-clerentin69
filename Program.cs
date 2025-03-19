4dfcf7ebc8659b6ef55a999cc98539e4d134e4b7
alexis1546 <pvaldemaralexis@gmail.com>
2025-03-19 10:47:57 +0100

    documentation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
+ * Documentation
+ * Entrée: (int) nombre de minutes
+ * Sortie : (tuple) le temps en heure et en minute
+ * Retourne le temps en un format agréable
+ *  */
+
+
 //(Desoler on avait pas vu que fallait pas le faire, on va simuler qu'on l'a pas fait
 //int nb_min = int.Parse(Console.ReadLine());
 Console.WriteLine("Entrez un nb de minutes");
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # td-clerentin69
+**Programme minute**
+
+_Demande le nombre de minutes et retourne un format en heure:minute_
