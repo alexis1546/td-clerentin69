c6e1c6e3de571fe4157a040dbd2c2d6e5108dbfb
alexis1546 <pvaldemaralexis@gmail.com>
2025-03-19 10:56:17 +0100

    tuple fix

--- Program.cs
+++ Program.cs
@@ -6,12 +6,16 @@
  * Retourne le temps en un format agréable
  *  */
 
+static(int,int)  temps(int nb_min) {
+    (int,int) time = (0,0);
+    time.Item1 = nb_min / 60;
+    time.Item2 = nb_min % 60;
+    return time;
+}
 
 //(Desoler on avait pas vu que fallait pas le faire, on va simuler qu'on l'a pas fait
 //int nb_min = int.Parse(Console.ReadLine());
 Console.WriteLine("Entrez un nb de minutes");
 int nb_min =  int.Parse(Console.ReadLine());
-int[] time = {0,0};
-time[0] = nb_min / 60;
-time[1] = nb_min % 60;
-Console.WriteLine(time[0].ToString()+":"+time[1].ToString() );
+(int,int) res = temps(nb_min);
+Console.WriteLine(res.Item1.ToString()+":"+res.Item2.ToString() );
